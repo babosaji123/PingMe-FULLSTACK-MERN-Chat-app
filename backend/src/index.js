587b45852440443f54ee3a11397a45bb8a7164b6
babosaji123 <151328131+babosaji123@users.noreply.github.com>
2026-09-01 02:38:15 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173","https://pingmeeee.netlify.app",
+    origin: ["http://localhost:5173","https://pingmeeee.netlify.app"],
     credentials: true,
   })
 );
