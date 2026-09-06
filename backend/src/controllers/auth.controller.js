eb85da17f37df66b4860221a052d0346407ef273
babosaji123 <151328131+babosaji123@users.noreply.github.com>
2026-09-06 20:05:22 +0530

    Update auth.controller.js

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -62,7 +62,7 @@ export const login = async (req, res) => {
       return res.status(400).json({ message: "Invalid credentials" });
     }
 
-    cosnt token= generateToken(user._id, res);
+    const token= generateToken(user._id, res);
 
     res.status(200).json({
       _id: user._id,
