7bd182af3cdc197bdfa1b2f78e495e5b60b05fd9
babosaji123 <151328131+babosaji123@users.noreply.github.com>
2026-09-06 20:46:59 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "https://pingme-fullstack-mern-chat-app.onrender.com/api",
+  baseURL: import.meta.env.MODE === "development" ? "/api",
   withCredentials: true,
 });
 axiosInstance.interceptors.request.use((config) => {
