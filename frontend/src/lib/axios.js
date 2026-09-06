c275076ef2e208e0ad74697ee5be778f8a45ac36
babosaji123 <151328131+babosaji123@users.noreply.github.com>
2026-09-06 21:01:50 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,28 +1,14 @@
-// import axios from "axios";
-
-// export const axiosInstance = axios.create({
-//   baseURL: import.meta.env.MODE === "development" ? "/api",
-//   withCredentials: true,
-// });
-// axiosInstance.interceptors.request.use((config) => {
-//   const token = localStorage.getItem("token");
-//   if (token) {
-//     config.headers.Authorization = `Bearer ${token}`;
-//   }
-//   return config;
-// });
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" 
-    ? "http://localhost:5001/api" 
-    : "/api",
+  baseURL: import.meta.env.MODE === "development" ? "/api",
+  withCredentials: true,
 });
-
 axiosInstance.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) {
-    config.headers.Authorization = Bearer ${token};
+    config.headers.Authorization = `Bearer ${token}`;
   }
   return config;
 });
+
